9e88583ea8106c3c53676b05ee177130633e9e45
Kwayu Mmari <mmaridenis@gmail.com>
2024-08-23 02:56:23 +0300

    fixing get terms of use api

---
+++ lib/src/screens/models/expenses/expenses.dart
@@ -0,0 +1,60 @@
+import 'package:gugu/src/utils/app_const.dart';
+import 'package:gugu/src/widgets/app_base_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class expenses extends StatefulWidget {
+  const expenses({Key? key}) : super(key: key);
+
+  @override
+  State<expenses> createState() => _expensesState();
+}
+
+class _expensesState extends State<expenses> {
+  var fullname;
+  TextEditingController search = TextEditingController();
+  String searchQuery = '';
+  @override
+  void initState() {
+    getName();
+    super.initState();
+  }
+
+  Future<String> getName() async {
+    final SharedPreferences sharedPreferences =
+        await SharedPreferences.getInstance();
+    var name = sharedPreferences.getString('name');
+    setState(() {
+      fullname = name.toString();
+    });
+    return name.toString();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBaseScreen(
+        bgcolor: AppConst.white,
+        isvisible: false,
+        backgroundImage: false,
+        backgroundAuth: false,
+        padding: EdgeInsets.all(0),
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          backgroundColor: AppConst.primary,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(10),
+              bottomRight: Radius.circular(10),
+            ),
+          ),
+        ),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            
+          ],
+        ));
+  }
+}
