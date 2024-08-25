5cf93deb6a537904ddbbf511b60b0886277a0c7c
Kwayu Mmari <mmaridenis@gmail.com>
2024-08-25 10:37:50 +0300

    fixing get terms of use api

--- lib/src/gateway/categories.dart
+++ lib/src/gateway/categories.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:gugu/src/api/apis.dart';
 import 'package:flutter/material.dart';
 
@@ -40,12 +41,16 @@ class hairDressers {
 
   Future makeOrder(BuildContext context, String name, String phone,
       String hairStyleId, String hairDresserId, String randomNumber) async {
+    String branchId = dotenv.env['BRANCH_ID'] ?? '1';
+    String companyId = dotenv.env['COMPANY_ID'] ?? '1';
     Map<String, dynamic> dataValue = {
       'name': name,
       'phone': phone,
       'hairStyleId': hairStyleId,
       'hairDresserId': hairDresserId,
       'randomNumber': randomNumber,
+      'branchId': branchId,
+      'companyId': companyId
     };
     final response = await api.post(context, 'addOrder', dataValue);
     final decodedResponse = jsonDecode(response.body);
