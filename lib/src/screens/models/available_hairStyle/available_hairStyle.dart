c812af7a2059e319693427e76d799bdcbaacc1f9
Kwayu Mmari <mmaridenis@gmail.com>
2024-08-29 11:14:32 +0300

    fixing galleru part

--- lib/src/screens/models/available_hairStyle/available_hairStyle.dart
+++ lib/src/screens/models/available_hairStyle/available_hairStyle.dart
@@ -114,7 +114,7 @@ class _availableHairStylesState extends State<availableHairStyles> {
                                 color: Colors.grey.withOpacity(0.5),
                                 spreadRadius: 5,
                                 blurRadius: 7,
-                                offset: Offset(0, 3), // changes position of shadow
+                                offset: Offset(0, 3),
                               ),
                             ],
                             gradient: LinearGradient(
