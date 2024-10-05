abba3a91e8bc57cfb9c6d5bcf4cbcbb0dd633b83
Kwayu Mmari <mmaridenis@gmail.com>
2024-10-06 01:31:09 +0300

    deleted page module.css

--- lib/src/screens/models/bottomNavigationBar/bottomNavigationBar.dart
+++ lib/src/screens/models/bottomNavigationBar/bottomNavigationBar.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: file_names, prefer_const_constructors, prefer_const_literals_to_create_immutables, non_constant_identifier_names, prefer_typing_uninitialized_variables, avoid_print, curly_braces_in_flow_control_structures, depend_on_referenced_packages, library_private_types_in_public_api, import_of_legacy_library_into_null_safe
-
 import 'package:gugu/src/screens/models/dashboard/dashboard.dart';
 import 'package:gugu/src/screens/models/expenses/expenses.dart';
 import 'package:gugu/src/screens/models/payroll/payroll.dart';
